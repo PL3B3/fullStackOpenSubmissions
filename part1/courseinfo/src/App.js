7a8261c1210def1f9424ed928af727da29d87583
Kevin Qi <kqi30@gatech.edu>
2022-01-04 00:33:35 -0600

    Experimented in courseinfo
    Created unicafe, working on 1.6

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = (props) => {
   return (
@@ -56,6 +56,51 @@ const App = () => {
       <Content course={course} />
       <Total course={course} />
     </div>
+  )
+}
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App1 = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const incrementCounter = () => setCounter(counter + 1)
+  const decrementCounter = () => setCounter(counter - 1)
+  const resetCounter = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={decrementCounter} text='Minus 1'/>
+      <Button onClick={resetCounter} text='Reset'/>
+      <Button onClick={incrementCounter} text='Add 1'/>
+    </div>
+    
+  )
+}
+
+const App2 = () => {
+  const [ left, setLeft ] = useState(0)
+  const [ right, setRight ] = useState(0)
+
+  const incrementLeft = () => setLeft(left + 1)
+  const incrementRight = () => setRight(right + 1)
+
+  return (
+    <div>
+      <Display counter={left} />
+      <Button onClick={incrementLeft} text='Left + 1'/>
+      <Display counter={right} />
+      <Button onClick={incrementRight} text='Right + 1'/>
+    </div>
+    
   )
 }
 
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+
+const App = () => {
+  return (
+    <div>
+      Greemting
+    </div>
+  )
+}
+
+
+export default App;
